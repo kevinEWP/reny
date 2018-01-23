66852482cfc5e15de6a79b2ba430c2414b1f7a50
kevinEWP <32240018+kevinEWP@users.noreply.github.com>
2018-01-23 22:27:27 +0800

    Add files via upload

---
+++ reny/scripts/main.js
@@ -0,0 +1,27 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/flower.jpg') {
+      myImage.setAttribute ('src','images/dewae.jpg');
+    } else {
+      myImage.setAttribute ('src','images/flower.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = '你好, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
+
